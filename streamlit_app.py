0c8c7b853225242d0212ca85fe981f577cf9b059
JANANI <83290635+4VV18IS038@users.noreply.github.com>
2023-02-13 13:16:50 +0530

    Update streamlit_app.py
    added header
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('BREAKFAST MENU')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('BreakFast Menu')
+streamlit.text('Banana and oats meal')
+streamlit.text('ABC JUCIE')
+streamlit.text('Hard boiled free range EGG')
+streamlit.text('Vegetable salad and mixed Chocolate Pancake')
+
